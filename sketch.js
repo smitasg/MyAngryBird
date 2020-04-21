a7ca033c0dc8d7cc83d5cc58f72365f6627dd05b
smitasg <smitabhandare@gmail.com>
2020-04-21 18:46:11 +0530

    Changed the bird size

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg ;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,7 +33,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,200);
 
 }
 
